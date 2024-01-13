1ed748810364c83839bd89870278705aa3a7806d
rafaelcastrobr <rafaelcastrobr@gmail.com>
2024-01-13 20:25:57 -0300

    fix: alteracao dos codigos

--- src/Pages/Romantica/Desculpas/DesculpasRomanticaFem.jsx
+++ src/Pages/Romantica/Desculpas/DesculpasRomanticaFem.jsx
@@ -15,19 +15,19 @@ export default function DesculpasRomanticaFem() {
       <h3>Romântica | voz feminina</h3>
       <ContainerCss>
         <div>
-          <label>COD: 355</label>
+          <label>COD: 362</label>
           <Player link={ReplaceWord('https://drive.google.com/file/d/1LOzr9Mw1VeQfTcUEIYfO-QJgzq4KNzBp/view?usp=sharing')} />
         </div>
         <div>
-          <label>COD: 356</label>
+          <label>COD: 361</label>
           <Player link={ReplaceWord('https://drive.google.com/file/d/1F_pVSLL-k-SLkQXB8Vq2TZRtWyRfBLIa/view?usp=sharing')} />
         </div>
         <div>
-          <label>COD: 357</label>
+          <label>COD: 360</label>
           <Player link={ReplaceWord('https://drive.google.com/file/d/1_G9bNSN1Vxweh22sVe3Ww3SLlWQdFj1X/view?usp=sharing')} />
         </div>
         <div>
-          <label>COD: 358</label>
+          <label>COD: 359</label>
           <Player link={ReplaceWord('https://drive.google.com/file/d/1g9MH2AMNSe2T9FZ9N63RpQ7cEdsvZ6kI/view?usp=sharing')} />
         </div>
         
