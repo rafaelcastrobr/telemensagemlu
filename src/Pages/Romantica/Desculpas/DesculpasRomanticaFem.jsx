8b8b1fda74610fbbbc8d480a03370b525e1e963b
rafaelcastrobr <rafaelcastrobr@gmail.com>
2024-01-19 22:52:02 -0300

    troca servidor

--- src/Pages/Romantica/Desculpas/DesculpasRomanticaFem.jsx
+++ src/Pages/Romantica/Desculpas/DesculpasRomanticaFem.jsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import Player from "../../../Components/Player";
-import ReplaceWord from "../../../function/replace";
 
 const ContainerCss = styled.div`
   display: flex;
@@ -16,19 +15,19 @@ export default function DesculpasRomanticaFem() {
       <ContainerCss>
         <div>
           <label>COD: 362</label>
-          <Player link={ReplaceWord('https://drive.google.com/file/d/1LOzr9Mw1VeQfTcUEIYfO-QJgzq4KNzBp/view?usp=sharing')} />
+          <Player link={'https://public.dm.files.1drv.com/y4mDCoK2V0VfHcesH1zWvuK0IX2RbqVf8OTUz6kvvRGhL7_QfPTnQQHN0m9yEdVS4iO-4Nscpl-DDN6Vz0OZdmr42Q7pOzMoNne_FQGTBFsJSi_x2ppK_h_m111T3vz8a99aps0Dg6RJBYFr7qqzXnQdGHeLzxhHL-d-84R7pNyck4XanjA3WrCb7-qUlUdRIqM26Ni_W0WJ4TDnXSw1pnm5Qc58y27jJii2IC61PlyGu4?AVOverride=1'} />
         </div>
         <div>
           <label>COD: 361</label>
-          <Player link={ReplaceWord('https://drive.google.com/file/d/1F_pVSLL-k-SLkQXB8Vq2TZRtWyRfBLIa/view?usp=sharing')} />
+          <Player link={"https://public.dm.files.1drv.com/y4mXm2qPj6tlY0zzHxqxt_pqmkbGtRPUommT_-xrbVqQ53LK5V8xSVUkoho_edU0VpQSYPCfKUqSEdz9JPBD5Tg_K2U3MNpKNkVx3F7xG4EgHTgqU2sbmGkix3PeZDvtN4_r_rQARllXM0psacUiOM6-Z3khv6osD89i3fbRCaUZ5A-c44D6Qs1lawvQCAMi7VaoMWmT-Wkd-TmviINzAQzB0Fp3iXshT-AyZBN4vKyhbQ?AVOverride=1"} />
         </div>
         <div>
           <label>COD: 360</label>
-          <Player link={ReplaceWord('https://drive.google.com/file/d/1_G9bNSN1Vxweh22sVe3Ww3SLlWQdFj1X/view?usp=sharing')} />
+          <Player link={"https://public.dm.files.1drv.com/y4my86taZqRkRuAOwddGhNnh1XMUe3ufUPE1x-tXPl5Uf7V-AB0FReUsxbbYVKKWwFakN1svGddwsDcu-jy2nvWudzQp2O4j8SpXRCHP9SRCCuLWfNrg6D073SkbSmUeDWoDJvnBnWahhLkHHr1-iTv8Zw1MpHmtqnlXHFjuo_p3eGFl0FUOlz1Ns2gk69Z2FVGGHZPRQvbulFT1E8P_n-M0EXebuglYzELUAcsY1rWN2w?AVOverride=1"} />
         </div>
         <div>
           <label>COD: 359</label>
-          <Player link={ReplaceWord('https://drive.google.com/file/d/1g9MH2AMNSe2T9FZ9N63RpQ7cEdsvZ6kI/view?usp=sharing')} />
+          <Player link={"https://public.dm.files.1drv.com/y4mVXcCTC-wrsgOmMs25VXuiRPMOnB6njrB5pOQVnZHiQKgiGaQTws-T_nTgH2EpJyU_K3El54taJDNnkz9wes5iD_mBY2yi8fdKGYg1PfhLdo0AR_G499ARGPzrRUfBy9PUwU4I-3Nm5NGcn9Va5E4rOWep-0N3igrYAu8cyDUN_AVJ2wpXe_G_SXt-zCn7dWAB767xXa5Nmu0IcTaYR_rqZMzgBStn4R1uLaHp_synxU?AVOverride=1"} />
         </div>
         
         
