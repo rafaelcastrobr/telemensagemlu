1559ad528c66fa641d58f25b0d27d064270c4c63
rafaelcastroEvoSystems <rafael.castro@evosystems.com.br>
2024-02-11 12:53:43 -0300

    Update RomanticaInicial.jsx

--- src/Pages/Romantica/Neutra_hot/RomanticaFem.jsx
+++ src/Pages/Romantica/Neutra_hot/RomanticaFem.jsx
@@ -17,69 +17,69 @@ export default function RomanticaFem() {
       <ContainerCss>
         <div>
           <label>COD: 231</label>
-          <Player link={ReplaceWord('https://drive.google.com/file/d/1Xu8NbwzvXVq3rqIsy6QBv0952OoihTHm/view?usp=sharing')} />
+          <Player link={'https://whatsmensagem.s3.sa-east-1.amazonaws.com/telemensagem/romanticas/F+Neutra/231.mp3'} />
         </div>
         <div>
           <label>COD: 232</label>
-          <Player link={ReplaceWord('https://drive.google.com/file/d/1bH9wAhLigPD0S_1OmhEEHGjlDnS4yQJy/view?usp=sharing')} />
+          <Player link={'https://whatsmensagem.s3.sa-east-1.amazonaws.com/telemensagem/romanticas/F+Neutra/232.mp3'} />
         </div>
         <div>
           <label>COD: 233</label>
-          <Player link={ReplaceWord('https://drive.google.com/file/d/1bH9wAhLigPD0S_1OmhEEHGjlDnS4yQJy/view?usp=sharing')} />
+          <Player link={'https://whatsmensagem.s3.sa-east-1.amazonaws.com/telemensagem/romanticas/F+Neutra/233.mp3'} />
         </div>
         <div>
           <label>COD: 234</label>
-          <Player link={ReplaceWord('https://drive.google.com/file/d/1Di4gTqxwYptRNKZD6k18_G4tYNAzlnLY/view?usp=sharing')} />
+          <Player link={'https://whatsmensagem.s3.sa-east-1.amazonaws.com/telemensagem/romanticas/F+Neutra/234.mp3'} />
         </div>
         <div>
           <label>COD: 235</label>
-          <Player link={ReplaceWord('https://drive.google.com/file/d/1fVcqUi2IvtSI1YZEJ0l9d9fx7_dHxBh0/view?usp=sharing')} />
+          <Player link={'https://whatsmensagem.s3.sa-east-1.amazonaws.com/telemensagem/romanticas/F+Neutra/235.mp3'} />
         </div>
         <div>
           <label>COD: 236</label>
-          <Player link={ReplaceWord('https://drive.google.com/file/d/14WzT3rJXVFe2ezsjKU1qSEorPVwDCdw1/view?usp=sharing')} />
+          <Player link={'https://whatsmensagem.s3.sa-east-1.amazonaws.com/telemensagem/romanticas/F+Neutra/236.mp3'} />
         </div>
         <div>
           <label>COD: 237</label>
-          <Player link={ReplaceWord('https://drive.google.com/file/d/1sA5XDp434h3OdagonxJMljfYWHrw65R5/view?usp=sharing')} />
+          <Player link={'https://whatsmensagem.s3.sa-east-1.amazonaws.com/telemensagem/romanticas/F+Neutra/237.mp3'} />
         </div>
         <div>
           <label>COD: 238</label>
-          <Player link={ReplaceWord('https://drive.google.com/file/d/1UmMNiFAwVfO-WWloQQ48k8GKtd8EM2Gt/view?usp=sharing')} />
+          <Player link={'https://whatsmensagem.s3.sa-east-1.amazonaws.com/telemensagem/romanticas/F+Neutra/238.mp3'} />
         </div>
         <div>
           <label>COD: 239 </label>
-          <Player link={ReplaceWord('https://drive.google.com/file/d/1pZqXisZYGkJXY3Yj4oWjJVA9YKNRGMov/view?usp=sharing')} />
+          <Player link={'https://whatsmensagem.s3.sa-east-1.amazonaws.com/telemensagem/romanticas/F+Neutra/239.mp3'} />
         </div>
         <div>
           <span><img src={pimenta} alt="" /></span>
           <label>COD: 240</label>
-          <Player link={ReplaceWord('https://drive.google.com/file/d/17I5n38aSX4RSm_NNf2VauyMGZj7dlm9V/view?usp=sharing')} />
+          <Player link={'https://whatsmensagem.s3.sa-east-1.amazonaws.com/telemensagem/romanticas/F+Neutra/240p.mp3'} />
         </div>
         <div>
           <span><img src={pimenta} alt="" /></span>
           <label>COD: 241 </label>
-          <Player link={ReplaceWord('https://drive.google.com/file/d/1dujO5IiX3Sr5sXR2A53l8R4BLRg2FpsB/view?usp=sharing')} />
+          <Player link={'https://whatsmensagem.s3.sa-east-1.amazonaws.com/telemensagem/romanticas/F+Neutra/241p.mp3'} />
         </div>
         <div>
           <span><img src={pimenta} alt="" /></span>
           <label>COD: 242</label>
-          <Player link={ReplaceWord('https://drive.google.com/file/d/1bv6Q2VTrF4Jv_1TYlM9BXDtVZtsPCACm/view?usp=sharing')} />
+          <Player link={'https://whatsmensagem.s3.sa-east-1.amazonaws.com/telemensagem/romanticas/F+Neutra/242p.mp3'} />
         </div>
         <div>
           <span><img src={pimenta} alt="" /></span>
           <label>COD:  243</label>
-          <Player link={ReplaceWord('https://drive.google.com/file/d/1jVn5WaWa4EIzsy5IuQGk_jbomDYUKG39/view?usp=sharing')} />
+          <Player link={'https://whatsmensagem.s3.sa-east-1.amazonaws.com/telemensagem/romanticas/F+Neutra/243p.mp3'} />
         </div>
         <div>
           <span><img src={pimenta} alt="" /></span>
           <label>COD: 244 </label>
-          <Player link={ReplaceWord('https://drive.google.com/file/d/1HvkW1k8xYSqXpZgGrwrMyFnyig11oJeI/view?usp=sharing')} />
+          <Player link={'https://whatsmensagem.s3.sa-east-1.amazonaws.com/telemensagem/romanticas/F+Neutra/244p.mp3'} />
         </div>
         <div>
           <span><img src={pimenta} alt="" /></span>
           <label>COD: 245</label>
-          <Player link={ReplaceWord('https://drive.google.com/file/d/1FJOzMKkX9CoDtUK823us5EstyF182Kw9/view?usp=sharing')} />
+          <Player link={'https://whatsmensagem.s3.sa-east-1.amazonaws.com/telemensagem/romanticas/F+Neutra/245p.mp3'} />
         </div>
       </ContainerCss>
     </div>
--- src/Pages/Romantica/Neutra_hot/Romanticas.jsx
+++ src/Pages/Romantica/Neutra_hot/Romanticas.jsx
@@ -1,10 +1,6 @@
 import { Link } from "react-router-dom";
 
 /*
-<div>
-      <Link to='feminino'><button className='btn_entrar_appcss fem'>FEMININO RÔMANTICA</button></Link>
-      </div>
-
 
 
 
@@ -14,7 +10,10 @@ export default function Romanticas() {
     <div>
       <h1>Clique no gênero</h1>
 
-      
+      <div>
+      <Link to='feminino'><button className='btn_entrar_appcss fem'>FEMININO RÔMANTICA</button></Link>
+      </div>
+
 
       <div>
       <Link to='masculino'><button className='btn_entrar_appcss masc'>MASCULINO RÔMANTICA</button></Link>
